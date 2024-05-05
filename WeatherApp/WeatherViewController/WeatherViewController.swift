cf833c785d74bd860e39f75bcd21fddd13968661
Nureles Abdullaev <nureles.abdullaiuulu.ss@gmail.com>
2024-05-04 19:43:37 -0500

    add location title design

--- WeatherApp/WeatherViewController/WeatherViewController.swift
+++ WeatherApp/WeatherViewController/WeatherViewController.swift
@@ -10,9 +10,54 @@ import SnapKit
 
 final class WeatherViewController: UIViewController {
     
+    private let backgroundImage = UIImageView()
+    private let titleContainer = UIView()
+    private let titleView = TitleView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        setupBackgroundImage()
+        setupTitleContainer()
+        setupTitleView()
+    }
+    
+    private func setupBackgroundImage() {
+        view.addSubview(backgroundImage)
+        backgroundImage.contentMode = .scaleAspectFill
+        backgroundImage.image = UIImage(named: "clearSky")
+
+        backgroundImage.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    private func setupTitleContainer() {
+        view.addSubview(titleContainer)
+
+        titleContainer.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(titleContainer.snp.width).multipliedBy(0.7)
+        }
+    }
+
+    private func setupTitleView() {
+        titleContainer.addSubview(titleView)
+        titleView.setup(
+            TitleView.InputModel(title: "Bishkek",
+                                 subtitle: "Center".uppercased(),
+                                 currentTemp: 21,
+                                 description: "Clear sky",
+                                 highTemp: 25,
+                                 lowTemp: 16)
+        )
+
+        titleView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(20)
+        }
     }
 }
